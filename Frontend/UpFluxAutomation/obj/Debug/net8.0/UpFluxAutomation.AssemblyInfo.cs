655efa3ba0bd0612a4ae6f01d55a5f3a0efad11d
Erlinguss <D00251914@student.dkit.ie>
2024-12-04 14:07:22 +0000

    Deleted Empty Folder due git not recognize them

--- Frontend/UpFluxAutomation/obj/Debug/net8.0/UpFluxAutomation.AssemblyInfo.cs
+++ Frontend/UpFluxAutomation/obj/Debug/net8.0/UpFluxAutomation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UpFluxAutomation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be70219e27464669f60e93885a0a4a3f76240176")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+925cd636bfa4134568fb4494f09e95d155758454")]
 [assembly: System.Reflection.AssemblyProductAttribute("UpFluxAutomation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UpFluxAutomation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Frontend/UpFluxAutomationTest/obj/Debug/net8.0/UpFluxAutomationTest.AssemblyInfo.cs
+++ Frontend/UpFluxAutomationTest/obj/Debug/net8.0/UpFluxAutomationTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UpFluxAutomationTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be70219e27464669f60e93885a0a4a3f76240176")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+925cd636bfa4134568fb4494f09e95d155758454")]
 [assembly: System.Reflection.AssemblyProductAttribute("UpFluxAutomationTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UpFluxAutomationTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
