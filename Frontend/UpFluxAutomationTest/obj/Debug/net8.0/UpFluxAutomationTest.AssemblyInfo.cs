23f2bcfdee539bc8958087304333c57d813a579a
Erlinguss <D00251914@student.dkit.ie>
2024-12-05 15:29:12 +0000

    leaved .gitignore in the root file

--- Frontend/UpFluxAutomationTest/obj/Debug/net8.0/UpFluxAutomationTest.AssemblyInfo.cs
+++ Frontend/UpFluxAutomationTest/obj/Debug/net8.0/UpFluxAutomationTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UpFluxAutomationTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb21f913ae4f6d8584362469b31e2c20974e0400")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b531b9d47a26a5b9fef4c0ae33c54a04cacfc8a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UpFluxAutomationTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UpFluxAutomationTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
