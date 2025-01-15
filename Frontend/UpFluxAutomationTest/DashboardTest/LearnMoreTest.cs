64c961b64ea5d19c73e4ac5eb8234661dd2b904e
Erlinguss <D00251914@student.dkit.ie>
2025-01-15 22:30:50 +0000

    55578 - Implemented Lear More Test

---
+++ Frontend/UpFluxAutomationTest/DashboardTest/LearnMoreTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpFluxAutomation.Abstractions;
+using UpFluxAutomation.Models;
+using UpFluxAutomation.Steps;
+using UpFluxAutomationTest.Assertion;
+using UpFluxAutomationTest.TestBase;
+
+namespace UpFluxAutomation.DashboardTest
+{
+    [TestFixture]
+    public class LearnMoreTest : TestBase
+    {
+        [Test]
+        public async Task TestLearnMoreTest()
+        {
+            try
+            {
+                Console.WriteLine("Starting Learn More Test...");
+
+                // Create and initialize EngineerData
+                var engineerData = new EngineerData
+                {
+                    UpFluxEndPoint = BaseUrl,
+                    Email = EngineerEmail,
+                    EngineerToken = EngineerToken
+                };
+
+                Repository.Add(engineerData);
+
+                // Initialize the flow 
+                IStep flow = new CheckDashboardResponsiveness(Repository);
+                flow.Chain(new ClickLearnMoreButton(Repository));
+
+                // Execute the flow
+                await flow.Execute();
+
+                Console.WriteLine("Learn More flow executed successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occurred during Learn More Test: {ex.Message}");
+            }
+        }
+    }
+}
