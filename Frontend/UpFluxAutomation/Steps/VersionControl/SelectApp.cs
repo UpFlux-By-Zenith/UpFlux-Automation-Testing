80ae7e364529ca0aa9b7ca82a3343ac623b7c023
Erlinguss <D00251914@student.dkit.ie>
2025-01-17 22:18:50 +0000

    55578 - Implemented SelectApp and SelectAppVersion test steps

---
+++ Frontend/UpFluxAutomation/Steps/VersionControl/SelectApp.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Playwright;
+using System.Threading.Tasks;
+using UpFluxAutomation.Abstractions;
+using UpFluxAutomation.Models;
+
+namespace UpFluxAutomation.Steps.VersionControl
+{
+    public class SelectApp : BaseStep
+    {
+        public SelectApp(IRepository repository, IStep next = null) : base(repository, next) { }
+
+        protected override async Task PerformExecute()
+        {
+            Console.WriteLine("Select App option...");
+
+            var page = Repository.Get<IPage>();
+
+            var secondOptionLocator = page.Locator("role=listbox >> nth=1"); 
+            await secondOptionLocator.ClickAsync();
+
+            Console.WriteLine("App option selected...");
+        }
+    }
+}
+
---
+++ Frontend/UpFluxAutomation/Steps/VersionControl/SelectAppVersion.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Playwright;
+using System.Threading.Tasks;
+using UpFluxAutomation.Abstractions;
+using UpFluxAutomation.Models;
+
+namespace UpFluxAutomation.Steps.VersionControl
+{
+    public class SelectAppVersion : BaseStep
+    {
+        public SelectAppVersion(IRepository repository, IStep next = null) : base(repository, next) { }
+
+        protected override async Task PerformExecute()
+        {
+            Console.WriteLine("Select App option...");
+
+            var page = Repository.Get<IPage>();
+
+            var secondOptionLocator = page.Locator("role=listbox >> nth=1");
+            await secondOptionLocator.ClickAsync();
+
+            Console.WriteLine("App option selected...");
+        }
+    }
+}
+
