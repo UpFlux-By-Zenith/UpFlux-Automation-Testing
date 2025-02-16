11b29858d3958b782d417aa0044ec8f971a76eb1
Erlinguss <D00251914@student.dkit.ie>
2025-02-16 17:35:00 +0000

    55853 - Added Click Profile section test step

---
+++ Frontend/UpFluxAutomation/Steps/Profile/ClickProfileSection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Playwright;
+using System.Threading.Tasks;
+using UpFluxAutomation.Abstractions;
+using UpFluxAutomation.Models;
+
+namespace UpFluxAutomation.Steps.Profile
+{
+    public class ClickProfileSection : BaseStep
+    {
+        public ClickProfileSection(IRepository repository, IStep next = null) : base(repository, next) { }
+
+        protected override async Task PerformExecute()
+        {
+            Console.WriteLine("Clicking the Profile button...");
+
+            var page = Repository.Get<IPage>();
+
+            var profileButtonLocator = page.Locator("a[href='/account-settings']:has-text('Profile')");
+            await profileButtonLocator.WaitForAsync(new LocatorWaitForOptions { State = WaitForSelectorState.Visible });
+            await profileButtonLocator.ClickAsync();
+
+            Console.WriteLine("Profile button clicked successfully.");
+        }
+    }
+}
